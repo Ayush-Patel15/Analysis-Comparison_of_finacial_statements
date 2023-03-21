cf372094c7af803502069d43fc2f5c148c608c09
Ayush-Patel15 <patelayushap1512@gmail.com>
2023-03-21 11:32:48 +0530

    Scraper for balance sheet completed

--- src/scraper.py
+++ src/scraper.py
@@ -62,8 +62,61 @@ def scrape_income_statement_stock(income_statement_url_1, income_statement_url_2
         result["DIVIDEND"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
     return result
 
+def scrape_balance_sheet_stock(balance_sheet_url_1, balance_sheet_url_2):
+    result = {
+        "YEARS": [],
+        "SHAREHOLDERS FUND": {},
+        "NON-CURRENT LIABILITIES": {},
+        "CURRENT LIABILITIES": {},
+        "NON-CURRENT ASSETS": {},
+        "CURRENT ASSETS": {},
+        "CONTINGENT LIABILITIES": {},
+        "BONUS DETAILS": {},
+        "NON-CURRENT INVESTMENTS": {},
+        "CURRENT INVESTMENTS": {}
+    }
+    response_1 = get_soup(balance_sheet_url_1)
+    balance_section_1 = response_1.select(".financial-table")[0]
+    rows = balance_section_1.select("table tr")
+    result["YEARS"] = [td.text for td in rows[0].select("td")[1:-1]]
+    result["SHAREHOLDERS FUND"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[4:10]}
+    result["NON-CURRENT LIABILITIES"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[11:16]}
+    result["CURRENT LIABILITIES"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[17:23]}
+    result["NON-CURRENT ASSETS"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[25:34]}
+    result["CURRENT ASSETS"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[35:43]}
+    result["CONTINGENT LIABILITIES"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[45:46]}
+    result["BONUS DETAILS"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[47:48]}
+    result["NON-CURRENT INVESTMENTS"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[49:51]}
+    result["CURRENT INVESTMENTS"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[52:]}
+    #### scraping for page 2 ####
+    response_2 = get_soup(balance_sheet_url_2)
+    balance_section_2 = response_2.select(".financial-table")[0]
+    rows = balance_section_2.select("table tr")
+    result["YEARS"].extend([td.text for td in rows[0].select("td")[1:-1]])
+    for tr in rows[4:10]:
+        result["SHAREHOLDERS FUND"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
+    for tr in rows[11:16]:
+        result["NON-CURRENT LIABILITIES"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
+    for tr in rows[17:23]:
+        result["CURRENT LIABILITIES"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
+    for tr in rows[25:34]:
+        result["NON-CURRENT ASSETS"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
+    for tr in rows[35:43]:
+        result["CURRENT ASSETS"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
+    for tr in rows[45:46]:
+        result["CONTINGENT LIABILITIES"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
+    for tr in rows[47:48]:
+        result["BONUS DETAILS"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
+    for tr in rows[49:51]:
+        result["NON-CURRENT INVESTMENTS"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
+    for tr in rows[52:]:
+        result["CURRENT INVESTMENTS"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
+    return result
+
 if __name__ == "__main__":
     # print(get_soup(URL="https://www.moneycontrol.com/markets/indian-indices/changeTableData?exName=N&indicesID=9&selPage=marketTerminal"))
     # print(scrape_stocklist_from_indices(indices_url="https://www.moneycontrol.com/markets/indian-indices/changeTableData?exName=N&indicesID=9&selPage=marketTerminal"))
-    print(scrape_income_statement_stock("https://www.moneycontrol.com/financials/adanienterprises/consolidated-profit-lossVI/AE13/1", 
-            "https://www.moneycontrol.com/financials/adanienterprises/consolidated-profit-lossVI/AE13/2"))
+    # print(scrape_income_statement_stock("https://www.moneycontrol.com/financials/adanienterprises/consolidated-profit-lossVI/AE13/1", 
+    #         "https://www.moneycontrol.com/financials/adanienterprises/consolidated-profit-lossVI/AE13/2"))
+    print(scrape_balance_sheet_stock("https://www.moneycontrol.com/financials/adanienterprises/consolidated-balance-sheetVI/AE13/1", 
+            "https://www.moneycontrol.com/financials/adanienterprises/consolidated-balance-sheetVI/AE13/2"))
