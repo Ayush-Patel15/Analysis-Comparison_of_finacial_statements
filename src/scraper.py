466061f354d110ff1d63d532a9ed5ca08ac78dc6
Ayush-Patel15 <patelayushap1512@gmail.com>
2023-03-22 15:10:41 +0530

    corrected the income and balance sheet scrapers structure

--- src/scraper.py
+++ src/scraper.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 
-####### HELPER / GLOBAL FUNCTIONS #######
 ## function to get the soup of a webpage
 def get_soup(URL):
     response = BeautifulSoup(
@@ -25,92 +24,50 @@ def scrape_stocklist_from_indices(indices_url):
         })
     return result
 
+## function to get rows & heading rows from the main page url
+def get_rows_and_heading_rows(page_url):
+    response = get_soup(URL=page_url)
+    table_section = response.select(".financial-table")[0]
+    rows = table_section.select("table tr")
+    heading_rows = table_section.select("tr.darkbg")
+    return rows, heading_rows
+
+## function to scrape the details of income statement of a stock
 def scrape_income_statement_stock(income_statement_url_1, income_statement_url_2):
-    result = {
-        "YEARS": [],
-        "INCOME": {},
-        "EXPENSES": {},
-        "TAX EXPENSES-CONTINUED OPERATIONS": {},
-        "EARNINGS PER SHARE": {},
-        "DIVIDEND": {}
-    }
-    response_1 = get_soup(URL=income_statement_url_1)
-    income_section_1 = response_1.select(".financial-table")[0]
-    rows = income_section_1.select("table tr")
-    rows.pop(33)    # Popping the not required row from rows list
+    result = {}
+    rows, heading_rows = get_rows_and_heading_rows(income_statement_url_1)
     result["YEARS"] = [td.text for td in rows[0].select("td")[1:-1]]
-    result["INCOME"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[3:9]}
-    result["EXPENSES"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[10:22]}
-    result["TAX EXPENSES-CONTINUED OPERATIONS"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[23:33]}
-    result["EARNINGS PER SHARE"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[34:36]}
-    result["DIVIDEND"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[37:]}
-    #### scraping for page 2 ####
-    response_2 = get_soup(URL=income_statement_url_2)
-    income_section_2 = response_2.select(".financial-table")[0]
-    rows = income_section_2.select("table tr")
-    rows.pop(33)    # Popping the not required row from rows list
+    for row in rows[2:]:
+        if row not in heading_rows:
+            result[row.select("td")[0].text] = [td.text for td in row.select("td")[1:-1]]
+    ####### scraping for page 2 ########
+    rows, heading_rows = get_rows_and_heading_rows(income_statement_url_2)
     result["YEARS"].extend([td.text for td in rows[0].select("td")[1:-1]])
-    for tr in rows[3:9]:
-        result["INCOME"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
-    for tr in rows[10:22]:
-        result["EXPENSES"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
-    for tr in rows[23:33]:
-        result["TAX EXPENSES-CONTINUED OPERATIONS"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
-    for tr in rows[34:36]:
-        result["EARNINGS PER SHARE"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
-    for tr in rows[37:]:
-        result["DIVIDEND"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
+    for row in rows[2:]:
+        if row not in heading_rows:
+            try:
+                result[row.select("td")[0].text].extend([td.text for td in row.select("td")[1:-1]])
+            except Exception as e:
+                pass
     return result
 
+## function to scrape the details of balance sheet of a stock
 def scrape_balance_sheet_stock(balance_sheet_url_1, balance_sheet_url_2):
-    result = {
-        "YEARS": [],
-        "SHAREHOLDERS FUND": {},
-        "NON-CURRENT LIABILITIES": {},
-        "CURRENT LIABILITIES": {},
-        "NON-CURRENT ASSETS": {},
-        "CURRENT ASSETS": {},
-        "CONTINGENT LIABILITIES": {},
-        "BONUS DETAILS": {},
-        "NON-CURRENT INVESTMENTS": {},
-        "CURRENT INVESTMENTS": {}
-    }
-    response_1 = get_soup(balance_sheet_url_1)
-    balance_section_1 = response_1.select(".financial-table")[0]
-    rows = balance_section_1.select("table tr")
+    result = {}
+    rows, heading_rows = get_rows_and_heading_rows(balance_sheet_url_1)
     result["YEARS"] = [td.text for td in rows[0].select("td")[1:-1]]
-    result["SHAREHOLDERS FUND"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[4:10]}
-    result["NON-CURRENT LIABILITIES"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[11:16]}
-    result["CURRENT LIABILITIES"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[17:23]}
-    result["NON-CURRENT ASSETS"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[25:34]}
-    result["CURRENT ASSETS"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[35:43]}
-    result["CONTINGENT LIABILITIES"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[45:46]}
-    result["BONUS DETAILS"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[47:48]}
-    result["NON-CURRENT INVESTMENTS"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[49:51]}
-    result["CURRENT INVESTMENTS"] = {tr.select("td")[0].text: [td.text for td in tr.select("td")[1:-1]] for tr in rows[52:]}
+    for row in rows[2:]:
+        if row not in heading_rows:
+            result[row.select("td")[0].text] = [td.text for td in row.select("td")[1:-1]]
     #### scraping for page 2 ####
-    response_2 = get_soup(balance_sheet_url_2)
-    balance_section_2 = response_2.select(".financial-table")[0]
-    rows = balance_section_2.select("table tr")
+    rows, heading_rows = get_rows_and_heading_rows(balance_sheet_url_2)
     result["YEARS"].extend([td.text for td in rows[0].select("td")[1:-1]])
-    for tr in rows[4:10]:
-        result["SHAREHOLDERS FUND"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
-    for tr in rows[11:16]:
-        result["NON-CURRENT LIABILITIES"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
-    for tr in rows[17:23]:
-        result["CURRENT LIABILITIES"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
-    for tr in rows[25:34]:
-        result["NON-CURRENT ASSETS"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
-    for tr in rows[35:43]:
-        result["CURRENT ASSETS"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
-    for tr in rows[45:46]:
-        result["CONTINGENT LIABILITIES"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
-    for tr in rows[47:48]:
-        result["BONUS DETAILS"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
-    for tr in rows[49:51]:
-        result["NON-CURRENT INVESTMENTS"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
-    for tr in rows[52:]:
-        result["CURRENT INVESTMENTS"][tr.select("td")[0].text].extend([td.text for td in tr.select("td")[1:-1]])
+    for row in rows[2:]:
+        if row not in heading_rows:
+            try:
+                result[row.select("td")[0].text].extend([td.text for td in row.select("td")[1:-1]])
+            except Exception as e:
+                pass
     return result
 
 if __name__ == "__main__":
@@ -118,5 +75,9 @@ if __name__ == "__main__":
     # print(scrape_stocklist_from_indices(indices_url="https://www.moneycontrol.com/markets/indian-indices/changeTableData?exName=N&indicesID=9&selPage=marketTerminal"))
     # print(scrape_income_statement_stock("https://www.moneycontrol.com/financials/adanienterprises/consolidated-profit-lossVI/AE13/1", 
     #         "https://www.moneycontrol.com/financials/adanienterprises/consolidated-profit-lossVI/AE13/2"))
-    print(scrape_balance_sheet_stock("https://www.moneycontrol.com/financials/adanienterprises/consolidated-balance-sheetVI/AE13/1", 
-            "https://www.moneycontrol.com/financials/adanienterprises/consolidated-balance-sheetVI/AE13/2"))
+    # print(scrape_income_statement_stock("https://www.moneycontrol.com/financials/statebankindia/consolidated-profit-lossVI/SBI/1", 
+    #         "https://www.moneycontrol.com/financials/statebankindia/consolidated-profit-lossVI/SBI/2"))
+    # print(scrape_balance_sheet_stock("https://www.moneycontrol.com/financials/adanienterprises/consolidated-balance-sheetVI/AE13/1", 
+    #         "https://www.moneycontrol.com/financials/adanienterprises/consolidated-balance-sheetVI/AE13/2"))
+    print(scrape_balance_sheet_stock("https://www.moneycontrol.com/financials/statebankindia/consolidated-balance-sheetVI/SBI/1", 
+        "https://www.moneycontrol.com/financials/statebankindia/consolidated-balance-sheetVI/SBI/2"))
